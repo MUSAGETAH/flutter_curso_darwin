34d89aa22af9f2dd0feda7031991b9b92acbfebc
Sergio <granrah1@gmail.com>
2023-01-02 12:16:05 -0300

    cambios

--- lib/widgets/column.dart
+++ lib/widgets/column.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class myColumn extends StatelessWidget {
   const myColumn({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return DefaultTextStyle(
@@ -26,6 +27,7 @@ class myColumn extends StatelessWidget {
 
               // ignore: prefer_const_literals_to_create_immutables
               children: [
+               
                 // Text('HELLO'),
                 // const Text(
                 //   'WORLD',
